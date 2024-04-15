6853bbc9b9059887d300d19c8a6879366ad5384d
thawro <tomaszhawro.kontakt@gmail.com>
2024-04-15 13:17:42 +0200

    added resnet50 example

---
+++ models/resnet/load.py
@@ -0,0 +1,39 @@
+from torch import nn
+from torchvision.models.resnet import (
+    ResNet50_Weights,
+    ResNet152_Weights,
+    resnet50,
+    resnet152,
+)
+
+from src.engines.config import EngineConfig
+from src.engines.loader import ImageModelEngineLoader
+
+
+class BaseResnetEngineLoader(ImageModelEngineLoader):
+    name: str
+
+    def load_engine_cfg(self) -> EngineConfig:
+        engine_cfg = super().load_engine_cfg()
+        engine_cfg.name = self.name
+        return engine_cfg
+
+
+class Resnet152EngineLoader(BaseResnetEngineLoader):
+    name = "resnet152"
+
+    def load_pytorch_module(self) -> nn.Module:
+        module = resnet152(weights=ResNet152_Weights.DEFAULT)
+        module.fc = nn.Sequential(*[module.fc, nn.Softmax()])  # logits -> probs
+        module.eval()
+        return module
+
+
+class Resnet50EngineLoader(BaseResnetEngineLoader):
+    name = "resnet50"
+
+    def load_pytorch_module(self) -> nn.Module:
+        module = resnet50(weights=ResNet50_Weights.DEFAULT)
+        module.fc = nn.Sequential(*[module.fc, nn.Softmax()])  # logits -> probs
+        module.eval()
+        return module
